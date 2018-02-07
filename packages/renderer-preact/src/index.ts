2adb6dfc3d596297dfe881f655e4bcbe5511b5a4
Emil Rune Møller <emilrmoeller@gmail.com>
2018-02-07 11:30:43 +0100

    Added jsx declaration to enable custom elements in tsx

--- packages/renderer-preact/src/index.ts
+++ packages/renderer-preact/src/index.ts
@@ -1,5 +1,17 @@
 import { h, render } from 'preact';
 
+export type CssClassMap = { [className: string]: boolean };
+
+declare global {
+  namespace JSX {
+    interface Element {}
+    export interface IntrinsicElements {
+      // catch all
+      [tagName: string]: any;
+    }
+  }
+}
+
 export const withPreact = (base: any) => class extends base {
     public _renderOnPropertyChange = true;
 
