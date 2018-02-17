fb3ea782d6892546d0cceeff410a3f88c784f67f
Emil Rune Møller <emilrmoeller@gmail.com>
2018-02-18 00:42:30 +0100

    added a shared toVdom function that can be handy when working with preact and dynamically wanting to insert html elements from dom

--- packages/renderer-preact/src/index.ts
+++ packages/renderer-preact/src/index.ts
@@ -11,6 +11,21 @@ declare global {
   }
 }
 
+export const toVdom = (element: any, nodeName?: string) => {
+	if (element.nodeType === 3) return element.nodeValue;
+  if (element.nodeType !== 1) return null;
+  
+  let children=[], 
+      props={}, 
+      i=0, 
+      a=element.attributes, 
+      cn=element.childNodes;
+
+	for (i=a.length; i--; ) props[a[i].name] = a[i].value;
+	for (i=cn.length; i--; ) children[i] = toVdom(cn[i]);
+	return h(nodeName || element.nodeName.toLowerCase(), props, children);
+}
+
 export const withPreact = (base: any) => class extends base {
     public _renderOnPropertyChange = true;
 
