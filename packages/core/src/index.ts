bdce950fb9d2c67d30da3f1c513ec0610934d8a9
Emil Rune Møller <emilrmoeller@gmail.com>
2018-04-05 09:18:45 +0200

    added event emitter to index exports and updated versions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
 export { FrameElement } from './frame-element.js';
-export { CustomElement, Property, Attribute, Observe, Listen, Event } from './frame-decorators.js';
+export { CustomElement, Property, Attribute, Observe, Listen, Event, EventEmitter } from './frame-decorators.js';
